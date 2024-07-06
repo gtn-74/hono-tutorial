e42f0b347408588d4d105bd0a820535c1e9f660c
gtn-74 <gtn.r74@gmail.com>
2024-07-06 21:08:41 +0900

    [環境構築] re-install modules

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,37 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import { prettyJSON } from 'hono/pretty-json';
 
-const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const app = new Hono();
 
-export default app
+
+
+let blogPosts = [
+  {
+    id: "1",
+    title: "Blog1",
+    content: "Blog1",
+  },
+  {
+    id: "2",
+    title: "Blog2",
+    content: "Blog2",
+  },
+  {
+    id: "3",
+    title: "Blog3",
+    content: "Blog3",
+  },
+];
+app.use('*',prettyJSON())
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+// teturなくても返る？
+app.get("/posts", (c) => {
+  return c.json({ posts: blogPosts });
+});
+
+export default app;
